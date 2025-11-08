cf296b988c7ec007e62c9fbc02d409700c7835e7
Quoc Vuong <quocvuong99er@gmail.com>
2025-11-08 08:24:30 +0700

    update code

--- src/components/Cursor/useCanvasCursor.js
+++ src/components/Cursor/useCanvasCursor.js
@@ -105,7 +105,11 @@ const useCanvasCursor = () => {
       ctx.globalCompositeOperation = "source-over";
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.globalCompositeOperation = "lighter";
-      ctx.strokeStyle = "hsla(" + Math.round(f.update()) + ",50%,50%,0.2)";
+      // ctx.strokeStyle = "hsla(" + Math.round(f.update()) + ",50%,50%,0.2)";
+      ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
+      // #d91424, #dba102
+      // ctx.strokeStyle = "#d91424";
+
       ctx.lineWidth = 1;
       for (var e, t = 0; t < E.trails; t++) {
         (e = lines[t]).update();
